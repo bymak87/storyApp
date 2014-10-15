78ce09567ada5176a3991cb4a3e20df1d61ce751
Amy Bakameyer <bymak87@gmail.com>
2014-10-14 21:05:05 -0500

    added express

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello from node");
+var express = require('express'),
+    app     = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/client/views/index.html');
+});
+
+app.listen(3000, function (){
+  console.log("I'm listening...");
+});

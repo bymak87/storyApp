a7b9c542eabb06f41c89756378291a52c57d4592
Amy Bakameyer <bymak87@gmail.com>
2014-10-15 01:32:49 -0500

    added ui-router

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('storyApp', ['ngResource']);
+var app = angular
+.module('storyApp', [
+  'ngResource',
+  'ui.router'])
+
+.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('home',{
+    url:'/',
+    templateUrl: 'views/index.html',
+    controller: 'storiesController'
+  })
+  .state('about',{
+    url:'/about',
+    templateUrl: 'templates/about.html'
+  });
+}]);
